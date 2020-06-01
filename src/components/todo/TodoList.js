8db7c6662afe7275d9821662bbb3da6ad6603df7
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-06-01 23:37:38 +0530

    kanban board design added

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { API } from '../../services';
 import { DeleteTodo } from '../todo';
+import _ from 'lodash';
 
 export default function TodoList() {
 	const [todos, setTodos] = useState([]);
+	const [grouped, setGrouped] = useState();
+
 	useEffect(() => {
 		const CREDENTIALS = {
 			url: `todos`,
+			queryParams: {
+				all: true,
+			},
 			method: 'get',
 		};
 
@@ -14,6 +20,11 @@ export default function TodoList() {
 			.then((response) => {
 				console.log('response', response);
 				setTodos(response.data);
+
+				let grouped = _.mapValues(_.groupBy(response.data, 'label'));
+
+				console.log('grouped', grouped);
+				setGrouped(grouped);
 			})
 			.finally(() => {});
 	}, []);
@@ -21,21 +32,36 @@ export default function TodoList() {
 	return (
 		<div>
 			<p>table</p>
-			{todos && todos.length > 0 && (
+			{/* {todos && todos.length > 0 && (
 				<table className="todo__list__table">
-					{todos.map((data) => (
-						<tr key={data._id}>
-							<td>
-								<div>{data.title}</div>
-								<div>{data.description}</div>
-							</td>
-							<td>
-								<DeleteTodo data={data} />
-							</td>
-						</tr>
-					))}
+					<tbody>
+						{todos.map((data) => (
+							<tr key={data._id}>
+								<td>
+									<div>{data.title}</div>
+									<div>{data.description}</div>
+								</td>
+								<td>
+									<DeleteTodo data={data} />
+								</td>
+							</tr>
+						))}
+					</tbody>
 				</table>
-			)}
+			)} */}
+			<div className="kanban">
+				{grouped &&
+					Object.entries(grouped).map(([key, values], i) => (
+						<div key={i} className="kanban__group">
+							{values.map((data) => (
+								<div key={data._id} className="kanban__box">
+									<h4>{data.title}</h4>
+									<div>{data.description}</div>
+								</div>
+							))}
+						</div>
+					))}
+			</div>
 		</div>
 	);
 }
