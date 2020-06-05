acaf4d7a207cf7ce755ef37aedcf9ddf9bbb973e
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-06-05 10:29:41 +0530

    if session is valid, then redirect to todo done, previously it was in empty page

--- src/components/common/AuthRoute.js
+++ src/components/common/AuthRoute.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import { SESSION } from '../../services';
 
 function AuthRoute({ children, ...rest }) {
-	const { recentlyVisitedUrl } = useSelector((state) => state.history);
-
 	if (SESSION.isLoggedIn()) {
 		return (
 			<Redirect
 				to={{
-					pathname: recentlyVisitedUrl,
+					pathname: '/todo',
 				}}
 			/>
 		);
