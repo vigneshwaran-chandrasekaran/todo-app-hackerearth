b89b0e1df819fabd132b9406a6a6b8444ce31514
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-10-16 23:29:20 +0530

    using npm test now able to test function

--- src/__tests__/example.test.js
+++ src/__tests__/example.test.js
@@ -1,5 +1,17 @@
 import { add } from '../helpers/example';
 
 test('Adding 1 + 1 equals 2', () => {
-	expect(add(1, 1)).toBe(2);
+	expect(add(1, 23)).toBe(24);
+});
+
+test('Adding 1 + 1 equals 2', () => {
+	expect(add(1, '3')).toBe('13');
+});
+
+test('Adding 1 + 1 equals 2', () => {
+	expect(add('6', 5)).toBe('65');
+});
+
+test('Adding 1 + 1 equals 2', () => {
+	expect(add(7)).toBe(8);
 });
--- src/helpers/example.js
+++ src/helpers/example.js
@@ -1,3 +1,3 @@
-export function add(a, b) {
+export function add(a, b = 1) {
 	return a + b;
 }
